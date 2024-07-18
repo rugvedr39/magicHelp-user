9738264721c1f22cba5896ae2316f758e1a79c1f
Rugved-Technify <rugvedr39@gmail.com>
2024-07-18 17:06:37 +0530

    url changed backend
    url changed backend

--- mlm-frontend/src/environments/environment.prod.ts
+++ mlm-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://your-production-backend-url/api' // Replace with your production backend URL
+    apiUrl: 'http://ec2-13-210-45-74.ap-southeast-2.compute.amazonaws.com:3000' // Replace with your local backend URL
   };
