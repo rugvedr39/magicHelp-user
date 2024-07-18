3c5e663823a7d7656d77f113331b778377ebf84a
Rugved-Technify <rugvedr39@gmail.com>
2024-07-18 19:23:16 +0530

    dsc

--- mlm-frontend/src/environments/environment.prod.ts
+++ mlm-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https:big91club.com' // Replace with your local backend URL
+    apiUrl: 'https://big91club.com' // Replace with your local backend URL
   };
--- mlm-frontend/src/environments/environment.ts
+++ mlm-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https:big91club.com' // Replace with your local backend URL
+    apiUrl: 'https://big91club.com' // Replace with your local backend URL
     // apiUrl: 'http://localhost:3000'
   };
