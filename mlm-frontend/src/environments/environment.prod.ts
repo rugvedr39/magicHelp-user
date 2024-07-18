c5d92de4326c35f7df0348bbeefd955f875beca5
Rugved-Technify <rugvedr39@gmail.com>
2024-07-18 18:36:14 +0530

    ng build

--- mlm-frontend/src/environments/environment.prod.ts
+++ mlm-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://ec2-13-210-45-74.ap-southeast-2.compute.amazonaws.com:3000' // Replace with your local backend URL
+    apiUrl: 'https:big91club.com' // Replace with your local backend URL
   };
