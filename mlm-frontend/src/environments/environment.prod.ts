047cb119870b2d3fa925b53960eee3a6a0654628
Rugved-Technify <rugvedr39@gmail.com>
2024-07-18 17:05:48 +0530

    first commit

---
+++ mlm-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://your-production-backend-url/api' // Replace with your production backend URL
+  };
---
+++ mlm-frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://ec2-13-210-45-74.ap-southeast-2.compute.amazonaws.com:3000' // Replace with your local backend URL
+    // apiUrl: 'http://localhost:3000'
+  };
